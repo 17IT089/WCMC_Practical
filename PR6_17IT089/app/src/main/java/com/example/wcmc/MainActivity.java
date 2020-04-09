25d110d794bcd5b1c93b76db40d983d900626b6f
17IT089 <63409797+17IT089@users.noreply.github.com>
2020-04-09 21:24:42 +0530

    PR6 commit

---
+++ PR6_17IT089/app/src/main/java/com/example/wcmc/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.wcmc;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity; import android.os.Build;
+import android.os.Bundle; import android.widget.Toast;
+import android.widget.ToggleButton;
+import android.hardware.camera2.CameraManager;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.camera2.CameraAccessException;
+import android.widget.CompoundButton;
+
+@RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+public class MainActivity extends AppCompatActivity {
+
+    private CameraManager mCameraManager;
+    private String mCameraId;
+    private ToggleButton toggleButton;
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP) @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        boolean isFlashAvailable = getApplicationContext().getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
+        if (!isFlashAvailable) {
+        }
+        mCameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        try {
+            mCameraId = mCameraManager.getCameraIdList()[0];
+        }
+        catch (CameraAccessException e) { e.printStackTrace();
+        }
+        toggleButton = findViewById(R.id.toggleButton); toggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() { @RequiresApi(api = Build.VERSION_CODES.M) @Override
+        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) { switchFlashLight(isChecked); }});
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M) public void switchFlashLight(boolean status) {
+        try {
+            mCameraManager.setTorchMode(mCameraId, status); if(status){
+                Toast.makeText(getApplicationContext(),"FLASH ON",Toast.LENGTH_SHORT).show();
+            }
+            if(!status){ Toast.makeText(getApplicationContext(),"FLASH OFF",Toast.LENGTH_SHORT).show();
+            }
+
+        } catch (CameraAccessException e) { e.printStackTrace();
+        }
+    }
+}
